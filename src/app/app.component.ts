1d0ce39438b4e15749ab672c433676ee8eacc291
Jared Baldemar <baldelamar917@hotmail.com>
2025-03-03 02:04:12 -0600

    unos pequeños cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GuestNavbarComponent } from "./shared/components/navbar/guest-navbar/guest-navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GuestNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Integradora';
+  title = 'Roomio';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './views/welcome/index/index.component';
+import { RegisterComponent } from './views/auth/register/register.component';
+import { LoginComponent } from './views/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent
+    }
+    ,
+    {
+        path: 'Index',
+        component: IndexComponent
+    },
+    {
+        path: 'Login',
+        component: LoginComponent
+    },
+    {
+        path: 'Register',
+        component: RegisterComponent
+    }
+];
