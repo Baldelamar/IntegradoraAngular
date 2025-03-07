a3e0eac22ca8beeb8b7143612b41a969119bfac4
Cristian Mata <134813558+CrisMc00@users.noreply.github.com>
2025-03-07 17:24:24 -0600

    tostada de login

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -19,4 +19,39 @@ export class LoginComponent {
     password: new FormControl('', Validators.required),
   });
 
+  private getErrorMessage(campo: string, nombreCampo: string): string | null {
+    // OBTENEMOS EL CONTROL DEL CAMPO
+    const control = this.FormularioLogin.get(campo);
+    // SI EL CONTROL NO EXISTE O NO TIENE ERRORES RETORNAMOS NULL
+    if (!control || !control.errors) return null;
+
+    const errors = control.errors;
+
+    // DEPENDIENDO DEL TIPO DE ERROR RETORNAMOS UN MENSAJE
+    if (errors['required']) return `${nombreCampo}: Este campo es obligatorio`;
+    if (errors['minlength']) return `${nombreCampo}: Debe tener al menos ${errors['minlength'].requiredLength} caracteres`;
+    if (errors['maxlength']) return `${nombreCampo}: No puede tener más de ${errors['maxlength'].requiredLength} caracteres`;
+    if (errors['pattern']) return `${nombreCampo}: Solo se permiten letras y espacios`;
+    if (errors['min']) return `${nombreCampo}: Debe ser mayor a $${errors['min'].min}`;
+    if (errors['max']) return `${nombreCampo}: No puede ser mayor a $${errors['max'].max}`;
+    if (errors['email']) return `${nombreCampo}: tiene que ser un correo válido`;
+
+    return null;
+  }
+
+  onLogin() {
+    if (this.FormularioLogin.valid){
+
+    }
+    else {
+      const campos: { [key: string]: string } = {email: 'Email', password: 'Contraseña' };
+
+      Object.keys(campos).forEach((key) => {
+        const errorMessage = this.getErrorMessage(key, campos[key]);
+        if (errorMessage) {
+          this.tostada.error(errorMessage, 'Error de validación');
+        }
+      });
+    }
+  }
 }
