85a93c285980788fd245959d8ef8e2492f089df1
Jared Baldemar <baldelamar917@hotmail.com>
2025-03-07 14:14:46 -0600

    sobre romio modulo 100% terminado

--- src/app/views/welcome/index/index.component.ts
+++ src/app/views/welcome/index/index.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
